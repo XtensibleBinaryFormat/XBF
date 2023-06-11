38a132214e4ef9b91aa054ae3346da8a89f07b0c
David Krauthamer <dkrautha@pm.me>
2023-06-10 22:50:04 -0400

    Add serialize and deserialize for XdlVec

--- xdl_rs/src/xdl_vec/vec.rs
+++ xdl_rs/src/xdl_vec/vec.rs
@@ -1,9 +1,36 @@
-use crate::{XdlMetadata, XdlType};
+use crate::{DeserializeType, Serialize, XdlMetadata, XdlType};
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use std::io::{self, Read, Write};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct XdlVec {
     pub(super) inner_type: Box<XdlMetadata>,
     elements: Vec<XdlType>,
+}
+
+impl Serialize for XdlVec {
+    fn serialize(&self, writer: &mut impl Write) -> io::Result<()> {
+        writer.write_u16::<LittleEndian>(self.elements.len() as u16)?;
+        self.elements.iter().try_for_each(|e| e.serialize(writer))
+    }
+}
+
+impl DeserializeType for XdlVec {
+    fn deserialize_type(metadata: &XdlMetadata, reader: &mut impl Read) -> io::Result<XdlType> {
+        let len = reader.read_u16::<LittleEndian>()? as usize;
+        let mut elements = Vec::with_capacity(len);
+        for _ in 0..len {
+            elements.push(XdlType::deserialize_type(metadata, reader)?);
+        }
+        Ok(XdlType::Vec(
+            XdlVec::new(metadata.clone(), elements).map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    "All elements are not the same type",
+                )
+            })?,
+        ))
+    }
 }
 
 impl XdlVec {
