251c482e95d710f7c595d1e7447b8b1cadc578dc
ahuston-0 <aliceghuston@gmail.com>
2023-06-21 20:05:25 -0400

    Change rust source code from xdl to xbf
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- xbf_rs/src/conversions/base_type.rs
+++ xbf_rs/src/conversions/base_type.rs
@@ -1,92 +1,92 @@
 use crate::{
-    xdl_primitive::{XdlPrimitive, XdlPrimitiveMetadata},
-    xdl_struct::{XdlStruct, XdlStructMetadata},
-    xdl_vec::{XdlVec, XdlVecMetadata},
-    XdlMetadata, XdlMetadataUpcast, XdlType, XdlTypeUpcast,
+    xbf_primitive::{XbfPrimitive, XbfPrimitiveMetadata},
+    xbf_struct::{XbfStruct, XbfStructMetadata},
+    xbf_vec::{XbfVec, XbfVecMetadata},
+    XbfMetadata, XbfMetadataUpcast, XbfType, XbfTypeUpcast,
 };
 
-impl From<XdlPrimitiveMetadata> for XdlMetadata {
-    fn from(value: XdlPrimitiveMetadata) -> Self {
-        XdlMetadata::Primitive(value)
+impl From<XbfPrimitiveMetadata> for XbfMetadata {
+    fn from(value: XbfPrimitiveMetadata) -> Self {
+        XbfMetadata::Primitive(value)
     }
 }
-impl From<&XdlPrimitiveMetadata> for XdlMetadata {
-    fn from(value: &XdlPrimitiveMetadata) -> Self {
-        XdlMetadata::Primitive(*value)
+impl From<&XbfPrimitiveMetadata> for XbfMetadata {
+    fn from(value: &XbfPrimitiveMetadata) -> Self {
+        XbfMetadata::Primitive(*value)
     }
 }
-impl XdlMetadataUpcast for XdlPrimitiveMetadata {}
+impl XbfMetadataUpcast for XbfPrimitiveMetadata {}
 
-impl From<XdlVecMetadata> for XdlMetadata {
-    fn from(value: XdlVecMetadata) -> Self {
-        XdlMetadata::Vec(value)
+impl From<XbfVecMetadata> for XbfMetadata {
+    fn from(value: XbfVecMetadata) -> Self {
+        XbfMetadata::Vec(value)
     }
 }
-impl From<&XdlVecMetadata> for XdlMetadata {
-    fn from(value: &XdlVecMetadata) -> Self {
-        XdlMetadata::Vec(value.clone())
+impl From<&XbfVecMetadata> for XbfMetadata {
+    fn from(value: &XbfVecMetadata) -> Self {
+        XbfMetadata::Vec(value.clone())
     }
 }
-impl XdlMetadataUpcast for XdlVecMetadata {}
+impl XbfMetadataUpcast for XbfVecMetadata {}
 
-impl From<XdlStructMetadata> for XdlMetadata {
-    fn from(value: XdlStructMetadata) -> Self {
-        XdlMetadata::Struct(value)
+impl From<XbfStructMetadata> for XbfMetadata {
+    fn from(value: XbfStructMetadata) -> Self {
+        XbfMetadata::Struct(value)
     }
 }
-impl From<&XdlStructMetadata> for XdlMetadata {
-    fn from(value: &XdlStructMetadata) -> Self {
-        XdlMetadata::Struct(value.clone())
+impl From<&XbfStructMetadata> for XbfMetadata {
+    fn from(value: &XbfStructMetadata) -> Self {
+        XbfMetadata::Struct(value.clone())
     }
 }
-impl XdlMetadataUpcast for XdlStructMetadata {}
+impl XbfMetadataUpcast for XbfStructMetadata {}
 
-impl From<&XdlType> for XdlMetadata {
-    fn from(value: &XdlType) -> Self {
+impl From<&XbfType> for XbfMetadata {
+    fn from(value: &XbfType) -> Self {
         match value {
-            XdlType::Primitive(x) => XdlMetadata::Primitive(x.into()),
-            XdlType::Vec(x) => XdlMetadata::Vec(x.into()),
-            XdlType::Struct(_x) => todo!(),
+            XbfType::Primitive(x) => XbfMetadata::Primitive(x.into()),
+            XbfType::Vec(x) => XbfMetadata::Vec(x.into()),
+            XbfType::Struct(_x) => todo!(),
         }
     }
 }
 
-impl From<XdlPrimitive> for XdlType {
-    fn from(value: XdlPrimitive) -> Self {
-        XdlType::Primitive(value)
+impl From<XbfPrimitive> for XbfType {
+    fn from(value: XbfPrimitive) -> Self {
+        XbfType::Primitive(value)
     }
 }
-impl From<&XdlPrimitive> for XdlType {
-    fn from(value: &XdlPrimitive) -> Self {
-        XdlType::Primitive(value.clone())
+impl From<&XbfPrimitive> for XbfType {
+    fn from(value: &XbfPrimitive) -> Self {
+        XbfType::Primitive(value.clone())
     }
 }
-impl XdlTypeUpcast for XdlPrimitive {}
+impl XbfTypeUpcast for XbfPrimitive {}
 
-impl From<XdlVec> for XdlType {
-    fn from(value: XdlVec) -> Self {
-        XdlType::Vec(value)
+impl From<XbfVec> for XbfType {
+    fn from(value: XbfVec) -> Self {
+        XbfType::Vec(value)
     }
 }
-impl From<&XdlVec> for XdlType {
-    fn from(value: &XdlVec) -> Self {
-        XdlType::Vec(value.clone())
+impl From<&XbfVec> for XbfType {
+    fn from(value: &XbfVec) -> Self {
+        XbfType::Vec(value.clone())
     }
 }
-impl XdlTypeUpcast for XdlVec {}
+impl XbfTypeUpcast for XbfVec {}
 
-impl From<XdlStruct> for XdlType {
-    fn from(value: XdlStruct) -> Self {
-        XdlType::Struct(value)
+impl From<XbfStruct> for XbfType {
+    fn from(value: XbfStruct) -> Self {
+        XbfType::Struct(value)
     }
 }
-impl From<&XdlStruct> for XdlType {
-    fn from(value: &XdlStruct) -> Self {
-        XdlType::Struct(value.clone())
+impl From<&XbfStruct> for XbfType {
+    fn from(value: &XbfStruct) -> Self {
+        XbfType::Struct(value.clone())
     }
 }
 // TODO: Test this
-impl XdlTypeUpcast for XdlStruct {}
+impl XbfTypeUpcast for XbfStruct {}
 
 #[cfg(test)]
 mod tests {
@@ -94,45 +94,45 @@ mod tests {
 
     #[test]
     fn test_upcast_metadata() {
-        let primitive_metadata = XdlPrimitiveMetadata::I32;
-        let vec_metadata = XdlVecMetadata::new(primitive_metadata.into());
+        let primitive_metadata = XbfPrimitiveMetadata::I32;
+        let vec_metadata = XbfVecMetadata::new(primitive_metadata.into());
         // TODO: Test Struct metadata once complete
 
         assert_eq!(
-            XdlMetadata::Primitive(primitive_metadata),
+            XbfMetadata::Primitive(primitive_metadata),
             (&primitive_metadata).to_base_metadata()
         );
         assert_eq!(
-            XdlMetadata::Primitive(primitive_metadata),
+            XbfMetadata::Primitive(primitive_metadata),
             primitive_metadata.into_base_metadata()
         );
 
         assert_eq!(
-            XdlMetadata::Vec(vec_metadata.clone()),
+            XbfMetadata::Vec(vec_metadata.clone()),
             (&vec_metadata).to_base_metadata()
         );
         assert_eq!(
-            XdlMetadata::Vec(vec_metadata.clone()),
+            XbfMetadata::Vec(vec_metadata.clone()),
             vec_metadata.into_base_metadata()
         );
     }
 
     #[test]
     fn test_upcast_type() {
-        let primitive_type = XdlPrimitive::I32(69);
+        let primitive_type = XbfPrimitive::I32(69);
         let vec_type =
-            XdlVec::new(XdlMetadata::Primitive((&primitive_type).into()), vec![]).unwrap();
+            XbfVec::new(XbfMetadata::Primitive((&primitive_type).into()), vec![]).unwrap();
         // TODO: Test Struct metadata once complete
 
         assert_eq!(
-            XdlType::Primitive(primitive_type.clone()),
+            XbfType::Primitive(primitive_type.clone()),
             (&primitive_type).to_base_type()
         );
         assert_eq!(
-            XdlType::Primitive(primitive_type.clone()),
+            XbfType::Primitive(primitive_type.clone()),
             primitive_type.into_base_type()
         );
-        assert_eq!(XdlType::Vec(vec_type.clone()), (&vec_type).to_base_type());
-        assert_eq!(XdlType::Vec(vec_type.clone()), vec_type.into_base_type());
+        assert_eq!(XbfType::Vec(vec_type.clone()), (&vec_type).to_base_type());
+        assert_eq!(XbfType::Vec(vec_type.clone()), vec_type.into_base_type());
     }
 }
