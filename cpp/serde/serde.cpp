9cb8ee88a4cb13825a58f056de1869e07b9e64a4
nancy gupta <ngupta19@stevens.edu>
2023-07-10 17:05:00 -0400

    put executables in bin directory and updated make file

--- cpp/serde/serde.cpp
+++ cpp/serde/serde.cpp
@@ -4,37 +4,13 @@
 #include <vector>
 #include <map>
 #include "rust.hh"
-#include <boost/archive/text_oarchive.hpp>
-#include <boost/archive/text_iarchive.hpp>
-#include <boost/serialization/vector.hpp>
-#include <boost/serialization/map.hpp>
-#include <boost/serialization/base_object.hpp>
-#include <boost/serialization/export.hpp>
+#include "metadata.hh"
 using namespace std;
 
 
 void dump(const stringstream& ss){
     // TODO: display all bytes in HEX, display the length, reset the buffer
 }
-enum class PrimitiveType {
-    Boolean,
-    U8,
-    U16,
-    U32,
-    U64,
-    U128,
-    U256,
-    I8,
-    I16,
-    I32,
-    I64,
-    I128,
-    I256,
-    F32,
-    F64,
-    Bytes,
-    String
-};
 
 class vec3d{
   private:
@@ -53,91 +29,6 @@ class vec3d{
 
 
 
-class Metadata {
-public:
-    virtual ~Metadata() {}
-    virtual PrimitiveType getType() const = 0; // Pure virtual function
-
-private:
-    friend class boost::serialization::access;
-
-    template <class Archive>
-    void serialize(Archive& ar, const unsigned int version) {}
-};
-
-class PrimitiveMetadata : public Metadata {
-public:
-    PrimitiveMetadata() {}
-    PrimitiveMetadata(PrimitiveType type) : type_(type) {}
-
-    PrimitiveType getType() const override {
-        return type_;
-    }
-
-private:
-    friend class boost::serialization::access;
-
-    template <class Archive>
-    void serialize(Archive& ar, const unsigned int version) {
-        ar & boost::serialization::base_object<Metadata>(*this);
-        ar & type_;
-    }
-
-    PrimitiveType type_;
-};
-
-class VecMetadata : public Metadata {
-public:
-    VecMetadata() {}
-    VecMetadata(const Metadata* internalType) : internalType_(internalType) {}
-
-    const Metadata* getInternalType() const {
-        return internalType_;
-    }
-
-    PrimitiveType getType() const override {
-        return PrimitiveType::U32; // Return the type of VecMetadata
-    }
-
-private:
-    friend class boost::serialization::access;
-
-    template <class Archive>
-    void serialize(Archive& ar, const unsigned int version) {
-        ar & boost::serialization::base_object<Metadata>(*this);
-        ar & internalType_;
-    }
-
-    const Metadata* internalType_;
-};
-
-class StructMetadata : public Metadata {
-public:
-    StructMetadata() {}
-
-    void addField(const std::string& name, const Metadata* fieldType) {
-        fields_[name] = fieldType;
-    }
-
-    const std::map<std::string, const Metadata*>& getFields() const {
-        return fields_;
-    }
-
-    PrimitiveType getType() const override {
-        return PrimitiveType::U8; // Return the type of StructMetadata
-    }
-
-private:
-    friend class boost::serialization::access;
-
-    template <class Archive>
-    void serialize(Archive& ar, const unsigned int version) {
-        ar & boost::serialization::base_object<Metadata>(*this);
-        ar & fields_;
-    }
-
-    std::map<std::string, const Metadata*> fields_;
-};
 
 BOOST_SERIALIZATION_ASSUME_ABSTRACT(Metadata)
 
