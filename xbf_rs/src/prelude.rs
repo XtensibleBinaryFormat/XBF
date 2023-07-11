03c7c9eac9b0d6f88cf3abb0b5b1a43e200b4f61
David Krauthamer <dkrautha@pm.me>
2023-07-11 20:44:18 +0000

    Add NativeToXbfPrimitive trait to prelude

--- xbf_rs/src/prelude.rs
+++ xbf_rs/src/prelude.rs
@@ -1,4 +1,4 @@
 //! The XBF prelude imports the various conversions traits to make creating xbf types as ergonomic
 //! as possible. The intention is to include `use xbf_rs::prelude::*;` and have easy access to the
 //! majority of things you'll need.
-pub use crate::{XbfMetadataUpcast, XbfTypeUpcast};
+pub use crate::{NativeToXbfPrimitive, XbfMetadataUpcast, XbfTypeUpcast};
