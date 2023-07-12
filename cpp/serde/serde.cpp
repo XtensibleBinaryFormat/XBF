0a2e1749dac56e32c0709cef9c312e18f78b7b7f
nancy gupta <ngupta19@stevens.edu>
2023-07-12 10:32:25 -0400

    checked hex value for other primitive data types, output is correct

--- cpp/serde/serde.cpp
+++ cpp/serde/serde.cpp
@@ -64,31 +64,50 @@ void test_serde_primitives(){
         u64 p4  = 8'000'000'000ULL;
         //U128
         //U256,
-        /*I8,
-        I16,
-        I32,
-        I64,
-        I128,
-        I256,
-        F32,
-        F64,
-        Bytes,
-        String*/
-    oa << b1 << p1 << p2 << p3 << p4;
+        i8 i1 = 127;
+        i16 i2 = 32767;
+        i32 i3 = 214748364;
+        i64 i4 = 9000000000000;
+       // I128,
+        //I256,
+        f32 f1 = 3.40282347e+36;
+        f64 f2 =  1.7976931348623157e+306;
+        byte b2 = static_cast<byte>(1);
+        string s1 = "hello";
+    //serialization
+    oa << b1 << p1 << p2 << p3 << p4 << i1 << i2 << i3 << i4 << f1 << f2 << b2 << s1;
+    //calling dump function
     dump(ss);
+    //deserialisation
     boost::archive::text_iarchive ia(ss);
     bool des_b1;
     u8 des_p1;
     u16 des_p2;
     u32 des_p3;
     u64 des_p4;
-    ia  >>  des_b1 >> des_p1 >> des_p2 >>des_p3 >> des_p4;
+    i8 des_i1;
+    i16 des_i2;
+    i32 des_i3;
+    i64 des_i4;
+    f32 des_f1;
+    f64 des_f2;
+    byte des_b2;
+    string des_s1;
+    
+    ia  >>  des_b1 >> des_p1 >> des_p2 >> des_p3 >> des_p4 >> des_i1 >> des_i2 >> des_i3 >> des_i4 >> des_f1>> des_f2 >> des_b2 >> des_s1;
     assert(b1 == des_b1);
     assert(p1 == des_p1);
     assert(p2 == des_p2);
     assert(p3 == des_p3);
     assert(p4 == des_p4);
-
+    assert(i1 == des_i1);
+    assert(i2 == des_i2);
+    assert(i3 == des_i3);
+    assert(i4 == des_i4);
+    assert(f1 == des_f1);
+    assert(f2 == des_f2);
+    assert(b2 == des_b2);
+    assert(s1 == des_s1);
 }
 
 int main() {
