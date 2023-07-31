a92bc6b39356613c9d172d353f67a7b08fa643c0
David Krauthamer <dkrautha@pm.me>
2023-07-31 00:30:54 -0400

    Update client example

--- xbf_rs/examples/client.rs
+++ xbf_rs/examples/client.rs
@@ -2,11 +2,11 @@ use anyhow::{Context, Result};
 use byteorder::WriteBytesExt;
 use std::{
     io::{BufReader, BufWriter, Write},
-    net::TcpStream,
+    net::{TcpStream, ToSocketAddrs},
 };
 use xbf_rs::{XbfMetadata, XbfPrimitive, XbfStruct, XbfType};
 
-fn try_find_galbatorix(value: &XbfType) -> Option<&XbfStruct> {
+fn find_galbatorix(value: &XbfType) -> Option<&XbfStruct> {
     if let XbfType::Vec(v) = value {
         for value in v {
             if let XbfType::Struct(s) = value {
@@ -21,18 +21,50 @@ fn try_find_galbatorix(value: &XbfType) -> Option<&XbfStruct> {
     None
 }
 
+fn get_connection(
+    addr: impl ToSocketAddrs,
+) -> Result<(BufWriter<TcpStream>, BufReader<TcpStream>)> {
+    let stream = TcpStream::connect(addr).context("Unable to connect to server")?;
+    let writer = BufWriter::new(stream.try_clone().context("Unable to clone stream")?);
+    let reader = BufReader::new(stream);
+    Ok((writer, reader))
+}
+
+fn get_data_and_metadata(addr: impl ToSocketAddrs) -> Result<(XbfMetadata, XbfType)> {
+    let (mut writer, mut reader) = get_connection(addr)?;
+    writer.write_u8(0)?;
+    writer.flush()?;
+    let metadata = XbfMetadata::deserialize_base_metadata(&mut reader)?;
+    let value = XbfType::deserialize_base_type(&metadata, &mut reader)?;
+    Ok((metadata, value))
+}
+
+fn get_data(addr: impl ToSocketAddrs, known_metadata: &XbfMetadata) -> Result<XbfType> {
+    let (mut writer, mut reader) = get_connection(addr)?;
+    writer.write_u8(1)?;
+    writer.flush()?;
+    let value = XbfType::deserialize_base_type(known_metadata, &mut reader)?;
+    Ok(value)
+}
+
+fn unknown_request(addr: impl ToSocketAddrs) -> Result<(XbfMetadata, XbfType)> {
+    let (mut writer, mut reader) = get_connection(addr)?;
+    writer.write_u8(2)?;
+    writer.flush()?;
+    let metadata = XbfMetadata::deserialize_base_metadata(&mut reader)?;
+    let value = XbfType::deserialize_base_type(&metadata, &mut reader)?;
+    Ok((metadata, value))
+}
+
 fn main() -> Result<()> {
-    let stream = TcpStream::connect("127.0.0.1:6969").context("Unable to connect")?;
-    let mut writer = BufWriter::new(stream.try_clone().context("Unable to clone stream")?);
-    let mut reader = BufReader::new(stream);
+    let (metadata, value) = get_data_and_metadata("127.0.0.1:6969")?;
+    println!("{:?}", find_galbatorix(&value));
 
-    writer.write_u8(0).context("Unable to write request type")?;
-    writer.flush().context("Unable to flush response")?;
+    let value = get_data("127.0.0.1:6969", &metadata)?;
+    println!("{:?}", find_galbatorix(&value));
 
-    let response_metadata = XbfMetadata::deserialize_base_metadata(&mut reader)?;
-    let response_value = XbfType::deserialize_base_type(&response_metadata, &mut reader)?;
-
-    println!("{:?}", try_find_galbatorix(&response_value));
+    let unnown_request = unknown_request("127.0.0.1:6969")?;
+    println!("{:?}", unnown_request);
 
     Ok(())
 }
